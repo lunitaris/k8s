f55711bd8abbd1a42b85e8785d8a72261434c38a
Lunitaris <mael.lepicard@gmail.com>
2024-09-27 10:09:47 +0200

    Update setup.sh correct apt repo

--- setup.sh
+++ setup.sh
@@ -41,13 +41,10 @@ apt-get install ca-certificates curl gnupg lsb-release -y
 #Note: We are not installing Docker Here.Since containerd.io package is part of docker apt repositories hence we added docker repository & it's key to download and install containerd.  
 # Add Docker’s official GPG key:  
 sudo mkdir -p /etc/apt/keyrings  
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  
-  
-#Use follwing command to set up the repository:  
-  
-echo \  
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \  
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
 ##############################################################################################  
 # Install containerd  
   
